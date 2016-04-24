60b99ca4679fea01be5f8295ce499fa0c0282c19
jackgb <209904@student.pwr.edu.pl>
2016-04-24 12:08:42 +0200

    Update array.cpp
    Pełna implementacja i interfejsem do pomiarów czasu.

--- array.cpp
+++ array.cpp
@@ -1,11 +1,24 @@
 #include <stdlib.h>
 #include <iostream>
-
+#include <fstream>
+#include <chrono>
 using namespace std;
+chrono::time_point<std::chrono::system_clock> start;
 
-			///////////////////////////
-			/* Implementacja tablicy */
-			///////////////////////////
+void timeStart(){
+	start = std::chrono::system_clock::now();
+}
+
+double timeEnd(){
+	chrono::time_point<std::chrono::system_clock> end;
+	end = chrono::system_clock::now();
+   	chrono::duration<double> elapsed_seconds = end-start;
+  	return elapsed_seconds.count();
+}
+
+	///////////////////////////
+	/* Implementacja tablicy */
+	///////////////////////////
 
 class ArrayElement{
 
@@ -84,10 +97,100 @@ class Array{
 				}
 			}
 		}	
+		
+		void clear(){
+			setSize(0);
+			array = (ArrayElement*)realloc(array, getSize() * sizeof(ArrayElement*));
+		}
 };
 
-
+class Interface{
+	
+	private:
+	
+		int amountOfElements[5] = {1000, 2000, 5000, 10000, 20000};
+		int amountOfTests = 100;
+		int elementCounter = 0;
+		Array array;
+	public:
+		
+		Interface(){
+			srand(time(NULL));
+		}
+		
+		void measurements(){
+			cout << "Measuring... Please hold." << endl;
+			fstream file; 
+			while(elementCounter <= 4){
+			cout << "Stage: " << elementCounter + 1 << endl;
+				for(int i = 0; i < amountOfTests; i++){
+					file.open("/home/jacek/projekt/array/arrayPushFront", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.push(rand(), 0);
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+ 					
+					file.open("/home/jacek/projekt/array/arrayPopFront", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.pop(0);
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+					
+					file.open("/home/jacek/projekt/array/arrayPushBack", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.push(rand(), array.getSize());
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+					
+					file.open("/home/jacek/projekt/array/arrayPopBack", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.pop(array.getSize());
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+ 					
+ 					file.open("/home/jacek/projekt/array/arrayPushRandom", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.push(rand(), rand() % amountOfElements[elementCounter]);
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+ 					
+ 					file.open("/home/jacek/projekt/array/arraySearch", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.find(rand());
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+ 					
+ 					
+ 					file.open("/home/jacek/projekt/array/arrayPopRandom", ios::out|ios::app);
+					timeStart();
+					for(int i = 0; i <= amountOfElements[elementCounter]; i++){
+						array.pop(rand() % amountOfElements[elementCounter]);
+					}
+					file << amountOfElements[elementCounter] << "		" << timeEnd() << endl;
+ 					file.close();
+ 					
+ 					array.clear();
+				}
+			elementCounter++;
+			}
+		}
+};
+	
 int main(){
 
+Interface interface;
+interface.measurements();
 return 0;
 }
